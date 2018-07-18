c0beab2d8911ad1aaa8679cdcc2ce757550d9a57
吴峰 <feng.wu@xbongbong.com>
2018-07-18 14:45:11 +0800

    修改操作方法

--- spring-cloud-auth/src/main/java/com/xbongbong/auth/AuthApplication.java
+++ spring-cloud-auth/src/main/java/com/xbongbong/auth/AuthApplication.java
@@ -13,11 +13,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @date: 2018/7/17 15:32
  * @desrcption:
  * 步骤：
- * 1、访问地址：http://localhost:1103/oauth/authorize?response_type=code&client_id=client&redirect_uri=http://baidu.com&state=123 跳转至登录界面；
+ * 1、访问地址：http://localhost:1103/oauth/authorize?response_type=code&client_id=client&redirect_uri=http://baidu.com&state=123 跳转至登录界面login.ftl；登录账号/密码：admin/123456
  * 2、登录成功之后到达authorize.ftl界面选择：approve
  * 3、会跳转至www.baidu.com，在链接尾部会有code字段；
  * 4、利用第3步的code字段，发送post请求，地址为：http://localhost:1103/oauth/token?client_id=client&grant_type=authorization_code&redirect_uri=http://baidu.com&code=Bp8Hq5
- * 5、获取返回的access_token
+ * 5、第一次需要输入账号和密码，分别为：client/secret，登录后，获取返回的access_token
  */
 @SpringBootApplication
 @EnableDiscoveryClient
