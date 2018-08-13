e030d42ed9eca6d62cfb4b4a1231d09e6728abce
吴峰 <feng.wu@xbongbong.com>
2018-08-13 15:00:24 +0800

    自定义注解

---
+++ paas/src/main/java/com/xbongbong/paas/constraints/ValidateApplicationSort.java
@@ -0,0 +1,29 @@
+package com.xbongbong.paas.constraints;
+
+import com.xbongbong.paas.validates.ApplicationSortValidate;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+/**
+ * @author: wufeng
+ * @date: 2018/8/13 9:42
+ * @desrcption: 应用排序的校验
+ */
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ApplicationSortValidate.class)
+public @interface ValidateApplicationSort {
+
+    String message();
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+}
---
+++ paas/src/main/java/com/xbongbong/paas/validates/ApplicationSortValidate.java
@@ -0,0 +1,33 @@
+package com.xbongbong.paas.validates;
+
+import com.xbongbong.paas.constraints.ValidateApplicationSort;
+import com.xbongbong.paas.dto.req.ApplicationSort;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+/**
+ * @author: wufeng
+ * @date: 2018/8/13 9:45
+ * @desrcption: 应用排序的校验的实现类
+ */
+public class ApplicationSortValidate implements ConstraintValidator<ValidateApplicationSort, List<ApplicationSort>> {
+
+    @Override
+    public void initialize(ValidateApplicationSort constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(List<ApplicationSort> value, ConstraintValidatorContext context) {
+        for(ApplicationSort applicationSort : value) {
+            Long id = applicationSort.getId();
+            Integer sort = applicationSort.getSort();
+            if (id == null || sort == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
